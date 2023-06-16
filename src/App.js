500581f790c6fd7442c86547f9696be56e847134
Creeper Panda T Rex <paintorange-nelson@outlook.com>
2023-06-16 11:13:11 -0700

    React npm init no create; Bubble Multiple chartjs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { AppBubble } from './BubbleChart';
+import {App1} from './MultipleCharts'
+
+export function App() {
+  return (
+    <>
+      <AppBubble/>;
+      <App1 />
+    </>
+  )
+}
